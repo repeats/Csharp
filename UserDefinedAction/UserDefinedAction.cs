70021e07f25122eed906137533d71d7ba02d4fb8
hptruong93 <hptruong93@gmail.com>
2016-09-09 20:47:09 -0400

    Initial commit

---
+++ UserDefinedAction/UserDefinedAction.cs
@@ -0,0 +1,18 @@
+﻿using Repeat.ipc;
+using System;
+using System.Collections.Generic;
+
+namespace Repeat.userDefinedAction {
+    public abstract class UserDefinedAction {
+
+        public RepeatClient controller { protected get; set; }
+
+        public List<int> invoker { protected get; set; }
+
+        public Activation activation { protected get; set; }
+
+        public string FileName { get; set; }
+
+        abstract public void Action();
+    }
+}
